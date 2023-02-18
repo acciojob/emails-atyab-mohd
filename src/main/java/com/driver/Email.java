742029abc77819a6392b041ab334c59740473598
simp-101 <mohdatyab12@gmail.com>
2023-02-18 18:06:40 +0530

    Second Assignment of february

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import javax.xml.stream.events.Characters;
+
 public class Email {
 
     private String emailId;
@@ -25,5 +27,37 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password)){
+            if(newPassword.length() >= 8){
+                if(upperCheck(newPassword) && lowerCheck(newPassword) && digitCheck(newPassword) && specialCheck(newPassword)){
+                    password = newPassword;
+                }
+            }
+        }
+    }
+    public boolean upperCheck(String pass){
+        //if(pass == null) return false;
+        for(int i=0;i<pass.length();i++){
+            if(pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z') return true;
+        }
+        return false;
+    }
+    public boolean lowerCheck(String pass){
+        for(int i=0;i<pass.length();i++){
+            if(pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z') return true;
+        }
+        return false;
+    }
+    public boolean digitCheck(String pass){
+        for(int i=0;i<pass.length();i++){
+            if(pass.charAt(i) >= '0' && pass.charAt(i) <= '9') return true;
+        }
+        return false;
+    }
+    public boolean specialCheck(String pass){
+        for(int i=0;i<pass.length();i++){
+            if(!(Character.isDigit(pass.charAt(i))) && !(Character.isAlphabetic(pass.charAt(i)))) return true;
+        }
+        return false;
     }
 }
